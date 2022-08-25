1ee20788bb48920a46b61b845a477d210105204d
lucas arthur <lucasarthur875@gmail.com>
2022-08-25 15:38:36 -0300

    primeiro commit

---
+++ ativadeparimpar.js
@@ -0,0 +1,13 @@
+let alunosPresentes = 32;
+
+for (let aluno = 0; aluno <= alunosPresentes; aluno++){
+    if(aluno == 0){
+        console.log("zero " + aluno);
+    }
+
+    if (aluno % 2 == 0) {//par
+        console.log ("par " + aluno);
+    } else {
+        console.log ("impar " + aluno);
+    }
+}
